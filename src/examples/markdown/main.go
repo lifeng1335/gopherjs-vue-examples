9fcbb7e5fd1f89c80d14c33443ab86228b078842
lifeng1335 <lifeng1335@outlook.com>
2017-09-11 23:09:26 +0800

    Add Support: Markdown Editor

---
+++ src/examples/markdown/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"vueutil"
+
+	"github.com/gopherjs/gopherjs/js"
+	"github.com/oskca/gopherjs-vue"
+)
+
+type AppData struct {
+	*js.Object
+	Input string `js:"input"`
+}
+
+type AppProps struct {
+	*js.Object
+}
+
+type App struct {
+	Date  *AppData
+	Props *AppProps
+}
+
+func NewApp(vm *vue.ViewModel) *App {
+	return &App{
+		Date: &AppData{
+			Object: vm.Data,
+		},
+		Props: &AppProps{
+			Object: vueutil.PropsData(vm),
+		},
+	}
+}
+
+func (app *App) SyncViewModel(vm *vue.ViewModel) {
+	keys := js.Keys(app.Date.Object)
+	for _, v := range keys {
+		vm.Data.Set(v, app.Date.Get(v))
+	}
+	vm.Get("$options").Set("propsData", app.Props.Object)
+}
+
+func main() {
+	o := vue.NewOption()
+	o.Data = NewAppData()
+	o.AddMethod("Update", func(vm *vue.ViewModel, args []*js.Object) {
+		app := NewApp(vm)
+		app.Update(args[0])
+		app.SyncViewModel(vm)
+	})
+	o.AddComputed("compiledMarkdown", func(vm *vue.ViewModel) interface{} {
+		app := NewApp(vm)
+		return app.CompiledMarkdown()
+	})
+
+	v := o.NewViewModel()
+	v.Mount("#editor")
+}
+
+func NewAppData() interface{} {
+	ad := &AppData{
+		Object: js.Global.Get("Object").New(),
+	}
+	ad.Input = `# hello`
+	return ad
+}
+
+func (app *App) Update(event *js.Object) {
+	//lodash := js.Global.Get("_")
+	//lodash.Call("debounce", func() {
+	//	m.Input = event.Get("target").Get("value").String()
+	//}, 300)
+	app.Date.Input = event.Get("target").Get("value").String()
+}
+
+func (app *App) CompiledMarkdown() *js.Object {
+	return js.Global.Call("marked", app.Date.Input, `{ sanitize: true }`)
+}
---
+++ src/vueutil/vueutil.go
@@ -0,0 +1,15 @@
+package vueutil
+
+import (
+	"github.com/gopherjs/gopherjs/js"
+	"github.com/oskca/gopherjs-vue"
+)
+
+func PropsData(vm *vue.ViewModel) *js.Object {
+	obj := js.Global.Get("Object").New()
+	keys := js.Keys(vm.Get("$options").Get("propsData"))
+	for _, value := range keys {
+		obj.Set(value, vm.Get(value))
+	}
+	return obj
+}
